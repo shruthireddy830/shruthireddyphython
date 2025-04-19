a9ae127ad92e62d6934ea71f5567be1baaee1100
shruthireddy830 <shruthireddy830@gmail.com>
2025-04-19 14:51:51 +0530

    Add files via upload

---
+++ personal information.py
@@ -0,0 +1,26 @@
+def get_person_details():
+"""
+function to read a person's details from the keyboard
+"""
+name=input("enter your name:")
+address=input("enter your address:")
+email=input("enter your email:")
+phone=input("enter your phone:")
+return name,address,email,phone
+
+def print_person_details(name,address,email,phone)
+"""
+funtion to print a person's details
+"""
+print("\n---personal details---")
+print(f"name:{name}")
+print(f"address:{address}")
+print(f"email:{email}")
+print(f"phone number:{phone number}")
+#get details from the user
+name,address,email,phone=get_person_details()
+#print the details
+print_person_details(name,address,email,phone number)
+
+
+                  
