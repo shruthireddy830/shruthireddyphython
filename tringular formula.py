f74855655da1d6a8cf8cd89e4ccdc29b5298e383
shruthireddy830 <shruthireddy830@gmail.com>
2025-04-19 14:52:20 +0530

    Add files via upload

---
+++ tringular formula.py
@@ -0,0 +1,6 @@
+#pythone program to print number in trangular form using for loop
+rows=int(input("entr number of rows:"))
+for i in range(rows,0,_1):
+    print("|n")
+    for i in range(rows-1+1):
+        print(i,"",end="")
